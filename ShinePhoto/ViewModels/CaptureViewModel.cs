7e50cd4bbcc5f6f0c61bf5fa2f94246970a16f64
Changweihua <changweihua@outlook.com>
2013-12-23 15:46:00 +0800

    Bug 修复，调整 MouseLeftButtonDown 事件为 MouseUp 事件

--- ShinePhoto/ViewModels/CaptureViewModel.cs
+++ ShinePhoto/ViewModels/CaptureViewModel.cs
@@ -6,7 +6,8 @@ using ShinePhoto.Interfaces;
 using System.ComponentModel.Composition;
 using Caliburn.Micro;
 using ShinePhoto.Models;
-using System.Windows.Media.Imaging;
+using System.Windows.Controls.Primitives;
+using System.Windows.Controls;
 
 namespace ShinePhoto.ViewModels
 {
@@ -15,8 +16,12 @@ namespace ShinePhoto.ViewModels
     {
         public BindableCollection<FileModel> FileModels { get; private set; }
 
-        public CaptureViewModel()
+        private readonly IEventAggregator _eventAggregator;
+
+        [ImportingConstructor]
+        public CaptureViewModel(IEventAggregator eventAggregator)
         {
+             _eventAggregator = eventAggregator;
             FileModels = new BindableCollection<FileModel>();
             
             double width, height;
@@ -32,7 +37,7 @@ namespace ShinePhoto.ViewModels
 
         void GetWH(string path, out double width, out double height)
         {
-            var bitmap = BitmapFrame.Create(new Uri(path, UriKind.RelativeOrAbsolute));
+            var bitmap = System.Windows.Media.Imaging.BitmapFrame.Create(new Uri(path, UriKind.RelativeOrAbsolute));
             double w = bitmap.Width;
             double h = bitmap.Height;
 
@@ -50,6 +55,77 @@ namespace ShinePhoto.ViewModels
             set {
                 _currentImageSource = value;
                 NotifyOfPropertyChange(() => CurrentImageSource);
+            }
+        }
+
+        public System.Drawing.Color _penColor;
+
+        public System.Drawing.Color PenColor
+        {
+            get { return _penColor; }
+            set {
+                _penColor = value;
+                NotifyOfPropertyChange(() => PenColor);
+            }
+        }
+
+        public void ImageTouched(object source, object parent)
+        {
+            var img = source as Image;
+            var sp = parent as UniformGrid;
+            if (img != null && img.Tag.ToString() == "0" && sp != null)
+            {
+                AdjustImageStaus(sp, img);
+            }
+        }
+
+        void AdjustImageStaus(UniformGrid sp, Image img)
+        {
+            foreach (var item in sp.Children)
+            {
+                if (item.GetType() == typeof(Image))
+                {
+                    var image = item as Image;
+
+                    if (image == img)
+                    {
+                        var path = img.Source.ToString();
+                        var name = path.Substring(0, path.LastIndexOf('.'));
+                        var ext = path.Substring(path.LastIndexOf('.'));
+                        img.Source = new System.Windows.Media.Imaging.BitmapImage(new Uri(name + "_Selected" + ext, UriKind.RelativeOrAbsolute));
+                        img.Tag = "1";
+                        continue;
+                    }
+
+                    var fileName = image.Source.ToString().Replace("_Selected", "");
+                    image.Source = new System.Windows.Media.Imaging.BitmapImage(new Uri(fileName, UriKind.RelativeOrAbsolute));
+                    image.Tag = "0";
+
+                }
+            }
+        }
+
+        public void ClearTouched(object source)
+        {
+            var image = source as Image;
+            if (image != null)
+            {
+                if (image.Tag.ToString() == "0")
+                {
+                    var path = image.Source.ToString();
+                    var name = path.Substring(0, path.LastIndexOf('.'));
+                    var ext = path.Substring(path.LastIndexOf('.'));
+                    image.Source = new System.Windows.Media.Imaging.BitmapImage(new Uri(name + "_Selected" + ext, UriKind.RelativeOrAbsolute));
+                    image.Stretch = System.Windows.Media.Stretch.UniformToFill;
+                    image.Tag = "1";
+                }
+                else
+                {
+                    var fileName = image.Source.ToString().Replace("_Selected", "");
+                    image.Source = new System.Windows.Media.Imaging.BitmapImage(new Uri(fileName, UriKind.RelativeOrAbsolute));
+                    image.Stretch = System.Windows.Media.Stretch.None;
+                    image.Tag = "0";
+                }
             }
         }
 
