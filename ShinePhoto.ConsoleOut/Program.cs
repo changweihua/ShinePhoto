64024cdba7c4942ee5167c356a17fdd4fb823b3e
Changweihua <changweihua@outlook.com>
2013-12-30 12:47:10 +0800

    ShinePhoto 控制台程序

---
+++ ShinePhoto.ConsoleOut/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ShinePhoto.ConsoleOut
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var flag = System.Text.RegularExpressions.Regex.IsMatch("{pack://application:,,,/ShinePhoto.Icons;Component/light/appbar.quill.png}", @"light");
+
+            Console.WriteLine(flag);
+
+            var result = System.Text.RegularExpressions.Regex.Replace("{pack://application:,,,/ShinePhoto.Icons;Component/light/appbar.quill.png}", "light", "dark");
+
+            Console.WriteLine(result);
+
+            Console.ReadKey(true);
+        }
+    }
+}
